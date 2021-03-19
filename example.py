6757ef3fd19bc23d136d03e781d04510041dfbe8
VanTalles <63775368+VanTalles@users.noreply.github.com>
2021-03-19 09:12:24 +0300

    Update example.py

--- example.py
+++ example.py
@@ -7,4 +7,12 @@ r1 = cd.connect()
 r3 = cd.get_classes_NetworkBox()
 print (r3)
 
+new_NetBox_card = {
+    "Code":"TEST_ITEM_api",
+    "Hostname":"TEST_ITEM_api",
+    "Availability":72, 
+}
+
+r4 = cd.
+
 r2 = cd.close()
